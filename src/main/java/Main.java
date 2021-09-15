7e39845bf259e457d4806acaa25d31ebd09d8f44
wettero1 <79643516+RogerWetter@users.noreply.github.com>
2021-09-15 17:09:39 +0200

    create project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Translator translator = new Translator("9afb67b1-b8e3-71eb-fd45-51b659629d18:fx");
+        translator.translate("Dieser Text sollte übersetzt werden.", Language.EN);
     }
 }
