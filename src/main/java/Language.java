d910fe0470690cf928d273a382afcc5ad9783852
wettero1 <79643516+RogerWetter@users.noreply.github.com>
2021-09-15 17:06:49 +0200

    create project

---
+++ src/main/java/Language.java
@@ -0,0 +1,29 @@
+/**
+ * "BG" - Bulgarian
+ * "CS" - Czech
+ * "DA" - Danish
+ * "DE" - German
+ * "EL" - Greek
+ * "EN" - English
+ * "ES" - Spanish
+ * "ET" - Estonian
+ * "FI" - Finnish
+ * "FR" - French
+ * "HU" - Hungarian
+ * "IT" - Italian
+ * "JA" - Japanese
+ * "LT" - Lithuanian
+ * "LV" - Latvian
+ * "NL" - Dutch
+ * "PL" - Polish
+ * "PT" - Portuguese (all Portuguese varieties mixed)
+ * "RO" - Romanian
+ * "RU" - Russian
+ * "SK" - Slovak
+ * "SL" - Slovenian
+ * "SV" - Swedish
+ * "ZH" - Chinese
+ */
+public enum Language {
+    BG, CS, DA, DE, EL, EN, ES, ET, FI, FR, HU, IT, JA, LT, LV, NL, PL, PT, RO, RU, SK, SL, SV, ZH
+}
